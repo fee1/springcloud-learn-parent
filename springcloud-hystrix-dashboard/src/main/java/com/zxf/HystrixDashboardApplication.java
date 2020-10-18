8557ea2f7ee8d7204d8fb6956bb5daaa331f1a35
zhuxiaofeng <1063446979@qq.com>
2020-10-18 22:43:36 +0800

    hystrix-dashboard断路器执行监控

---
+++ springcloud-hystrix-dashboard/src/main/java/com/zxf/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.zxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zxf
+ * 断路器执行监控
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
